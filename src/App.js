0cfd33e5337d1dc87b62e8eb4314a6bec7370167
julienbrg <julien@strat.cc>
2022-07-19 08:07:30 +0200

    add connect button

--- src/App.js
+++ src/App.js
@@ -1,37 +1,83 @@
-import logo from './inverted-dice-6.svg';
 import './App.css';
+import {useState} from 'react';
 import {
   Slider,
   SliderTrack,
   SliderFilledTrack,
   SliderThumb,
   SliderMark,
+  Button,
+  ButtonGroup,
+  Stack, HStack, VStack, Box
 } from '@chakra-ui/react'
+import { Body, Container, Header } from "./components/index.js";
+import { connect } from "get-starknet"
 
 function App() {
+
+  const [isConnected, setIsConnected] = useState(false);
+  const [bal, setBal] = useState(0)
+
+  async function startSession() {
+    console.log("start")
+    try {
+      const wallet = await connect({
+        include: ["braavos"],
+      });
+      if (wallet) {
+          await wallet.enable({ showModal: true });
+          setIsConnected(!!wallet?.isConnected);
+      }
+      console.log("isConnected: ", isConnected)
+    } catch {}
+  }
+
+  async function bet() {
+    console.log("bet")
+    setBal(bal+1)
+    // readTokenBalance().then(balance => setBal(balance ?? "Error"))
+    // ... 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Yo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    
+    <Container>
+      <Header>
+        <Button
+          
+          loadingText='Connecting'
+          colorScheme='blue'
+          variant='outline'
+          onClick={startSession}
         >
-          StarkBet
-        </a>
-        <Slider aria-label='slider-ex-2' colorScheme='pink' defaultValue={30}>
+          Connect
+        </Button>
+      </Header>
+      <Body>
+        <h1>
+          Balance: {bal} ETH
+        </h1>
+        
+        <Slider aria-label='slider-ex-2' colorScheme='pink' defaultValue={30} onChangeEnd={(val) => console.log(val)}>
           <SliderTrack>
             <SliderFilledTrack />
           </SliderTrack>
           <SliderThumb />
         </Slider>
-      </header>
-    </div>
+
+        <Stack direction='row' spacing={4}>
+
+        <Button
+          loadingText='Connecting'
+          colorScheme='blue'
+          variant='outline'
+          onClick={bet}
+        >
+        Bet
+        </Button>
+      </Stack>
+      </Body>
+    </Container>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+  align-items: center;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  font-size: calc(10px + 2vmin);
+  justify-content: center;
+  padding: 5% 20%;
+`;
+
+export const Button = styled.button`
+  background-color: white;
+  border: none;
+  border-radius: 8px;
+  color: #282c34;
+  cursor: pointer;
+  font-size: 16px;
+  margin: 0px 20px;
+  padding: 12px 24px;
+  text-align: center;
+  text-decoration: none;
+`;
+
+export const Container = styled.div`
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  height: calc(1000vh);
+  text-align: left;
+  
+`;
+
+export const Description = styled.div`
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  height: calc(1000vh);
+`;
+
+export const Details = styled.div`
+  
+`;
+
+export const Header = styled.header`
+  align-items: center;
+  background-color: #000000;
+  color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  min-height: 70px;
+  margin-right: 20px;
+`;
+
+export const Image = styled.img`
+  height: 40vmin;
+  margin-bottom: 10px;
+  pointer-events: none;
+  padding: 24px;
+`;
+
+export const Media = styled.img`
+  height: 60vmin;
+  margin-bottom: 10px;
+  pointer-events: none;
+`;
+
+export const Loader = styled.img`
+  pointer-events: none;
+`;
+
+export const Link = styled.a.attrs({
+  target: "_blank",
+  rel: "noopener noreferrer",
+})`
+  color: #805AD5;
+  margin-top: 8px;
+`;
+
